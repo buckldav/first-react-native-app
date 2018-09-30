d994175a7421c19ba263b619227d1d267e3f7839
buckldav <runalldayandnight@gmail.com>
2018-09-30 16:28:35 -0600

    Random hello, GET request

--- fullstack_template/static/js/Hello.jsx
+++ fullstack_template/static/js/Hello.jsx
@@ -1,22 +1,33 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
 var $ = require("jquery");
 
 export default class Hello extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { greeting: "Hello " + this.props.name };
+    this.state = { greeting: "Hello, " + this.props.name + "!" };
     // This binding is necessary to make `this` work in the callback
     this.getPythonHello = this.getPythonHello.bind(this);
   }
 
   getPythonHello() {
-    $.get(window.location.href + "hello", data => {
-      console.log(data);
-      this.personaliseGreeting(data);
-    });
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:5000/hello"
+    })
+      .done(data => {
+        console.log("Data: " + data);
+        this.personaliseGreeting(data);
+      })
+      .fail((xhr, textStatus, errorThrown) => {
+        alert(xhr.responseText);
+        alert(textStatus);
+      });
   }
 
   personaliseGreeting(greeting) {
-    this.setState({ greeting: greeting + " " + this.props.name + "!" });
+    this.setState({ greeting: greeting + ", " + this.props.name + "!" });
   }
 
   render() {
